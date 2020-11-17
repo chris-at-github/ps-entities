ad0760abc5dfbd1a70f1f5a57515d018d6d3127e
Christian Pschorr <c.pschorr@datamints.com>
2020-11-17 22:54:32 +0100

    Vorbereitungen Listing

--- Configuration/TCA/Overrides/tx_entities_domain_model_entity.php
+++ Configuration/TCA/Overrides/tx_entities_domain_model_entity.php
@@ -5,6 +5,6 @@ defined('TYPO3_MODE') || die();
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-   'entities',
-   'tx_entities_domain_model_entity'
+	'entities',
+	'tx_entities_domain_model_entity'
 );

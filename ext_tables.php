96f14f62a553da09975f033e330db121fa5a98dc
Christian Pschorr <c.pschorr@datamints.com>
2020-11-17 23:38:10 +0100

    Default Routing für Entities Extension hinterlegt

--- ext_tables.php
+++ ext_tables.php
@@ -8,7 +8,7 @@ call_user_func(
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'Entities',
             'Frontend',
-            'Frontend'
+            'Entities Frontend'
         );
 
 

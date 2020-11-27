b864fd8e7cfb8761a5675ee73788999315512517
Christian Pschorr <c.pschorr@datamints.com>
2020-11-27 22:09:05 +0100

    Aufräumarbeiten TCA

--- Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
+++ Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
@@ -1,9 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-
-
-
+// Kategorien definieren
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
    'entity',
    'tx_entity_domain_model_entity'

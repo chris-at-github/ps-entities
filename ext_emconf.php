069d0a887d9d0c7306e39eb97e43794be87d6a4a
Christian Pschorr <c.pschorr@datamints.com>
2020-12-02 09:57:07 +0100

    Bereinigung Entity emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,20 +11,21 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Entity',
-    'description' => '',
-    'category' => 'plugin',
-    'author' => 'Christian Pschorr',
-    'author_email' => 'pschorr.christian@gmail.com',
-    'state' => 'alpha',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '0.1.1',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
+	'title' => 'Entity',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'state' => 'alpha',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.1.1',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '10.4.0-10.4.99',
+			'xo' => '1.0.0-1.99.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
 ];

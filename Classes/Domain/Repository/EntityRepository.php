6d61b177337f42897d10773130df077d72c99a37
Christian Pschorr <c.pschorr@datamints.com>
2022-05-18 20:30:16 +0200

    Manuelle Auswahl von Records im Teaser Plugin implementiert

--- Classes/Domain/Repository/EntityRepository.php
+++ Classes/Domain/Repository/EntityRepository.php
@@ -36,6 +36,22 @@ class EntityRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 	protected function getMatches($query, $options) {
 		$matches = [];
 
+		if(isset($options['records']) === true) {
+
+			// bei Eingabe von festen IDs duerfen nur die IDs der Hauptsprache verwendet werden, Extbase kuemmert sich per
+			// Overlay um die korrekte Uebersetzung
+			// @see: https://docs.typo3.org/m/typo3/book-extbasefluid/master/en-us/9-CrosscuttingConcerns/1-localizing-and-internationalizing-an-extension.html#typo3-v9-and-higher
+			$query->getQuerySettings()->setRespectSysLanguage(false);
+			$query->getQuerySettings()->setLanguageOverlayMode(true);
+
+			// immer als Array auswerten
+			if(is_array($options['records']) === false) {
+				$options['records'] = [$options['records']];
+			}
+
+			$matches['records'] = $query->in('uid', $options['records']);
+		}
+
 		if(isset($options['masterCategory']) === true) {
 
 			// bei Eingabe von festen IDs duerfen nur die IDs der Hauptsprache verwendet werden, Extbase kuemmert sich per

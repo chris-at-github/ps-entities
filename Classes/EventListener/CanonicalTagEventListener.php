b2f4228fca45e6f1c2dc6ecc7c6e2e82d37d3b30
Christian Pschorr <pschorr.christian@gmail.com>
2024-06-08 16:50:17 +0200

    Übernahme CanonicalTagEventListener in Version 12

--- Classes/EventListener/CanonicalTagEventListener.php
+++ Classes/EventListener/CanonicalTagEventListener.php
@@ -42,32 +42,32 @@ class CanonicalTagEventListener {
 	 * @param ModifyUrlForCanonicalTagEvent $event
 	 */
 	public function __invoke(ModifyUrlForCanonicalTagEvent $event): void {
-//		$entity = $this->getEntity();
-//
-//		if($entity !== null) {
-//			if($entity->getNoIndex() === false) {
-//
-//				// gesetzte URL aus dem Enitity verwenden
-//				if(empty($entity->getCanonicalUrl()) === false) {
-//					$canonical = GeneralUtility::makeInstance(CanonicalTagService::class)->getUrl($entity->getCanonicalUrl());
-//
-//				// zur Sicherheit nochmal die Canonical setzen (falls das Entity unter mehreren URLs angezeigt wird
-//				} else {
-//					$uriOptions = [
-//						'absoluteUri' => true
-//					];
-//
-//					if($entity->getMasterCategory() !== null && $entity->getMasterCategory()->getPage() !== null) {
-//						$uriOptions['category'] = $entity->getMasterCategory();
-//					}
-//
-//					$canonical = $entity->getUri($uriOptions);
-//				}
-//
-//				if(empty($canonical) === false) {
-//					$event->setUrl($canonical);
-//				}
-//			}
-//		}
+		$entity = $this->getEntity();
+
+		if($entity !== null) {
+			if($entity->getNoIndex() === false) {
+
+				// gesetzte URL aus dem Enitity verwenden
+				if(empty($entity->getCanonicalUrl()) === false) {
+					$canonical = GeneralUtility::makeInstance(CanonicalTagService::class)->getUrl($entity->getCanonicalUrl());
+
+				// zur Sicherheit nochmal die Canonical setzen (falls das Entity unter mehreren URLs angezeigt wird
+				} else {
+					$uriOptions = [
+						'absoluteUri' => true
+					];
+
+					if($entity->getMasterCategory() !== null && $entity->getMasterCategory()->getPage() !== null) {
+						$uriOptions['category'] = $entity->getMasterCategory();
+					}
+
+					$canonical = $entity->getUri($uriOptions);
+				}
+
+				if(empty($canonical) === false) {
+					$event->setUrl($canonical);
+				}
+			}
+		}
 	}
 }

b502412ea7c9555006c35d5f1ff132225d3be007
Christian Pschorr <pschorr.christian@gmail.com>
2024-06-08 22:34:47 +0200

    XML Sitemap Generierung für Produkte (Entities) erstellt

--- Classes/XmlSitemap/RecordsXmlSitemapDataProvider.php
+++ Classes/XmlSitemap/RecordsXmlSitemapDataProvider.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Ps\Entity\XmlSitemap;
 
+use cogpowered\FineDiff\Parser\Operations\Delete;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Seo\XmlSitemap\Exception\MissingConfigurationException;
 
@@ -18,8 +20,8 @@ use TYPO3\CMS\Seo\XmlSitemap\Exception\MissingConfigurationException;
  */
 
 // auf Version 12 verschoben -> extends \TYPO3\CMS\Seo\XmlSitemap\RecordsXmlSitemapDataProvider fuehrt zu einem Fehler
-//class RecordsXmlSitemapDataProvider extends \TYPO3\CMS\Seo\XmlSitemap\RecordsXmlSitemapDataProvider {
-class RecordsXmlSitemapDataProvider {
+class RecordsXmlSitemapDataProvider extends \TYPO3\CMS\Seo\XmlSitemap\RecordsXmlSitemapDataProvider {
+//class RecordsXmlSitemapDataProvider {
 
 	/**
 	 * @var array
@@ -34,21 +36,35 @@ class RecordsXmlSitemapDataProvider {
 	 */
 	protected function getMasterCategoryPageUid(int $categoryUid): int {
 		if(empty($this->masterCategories) === true) {
-			$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('entity_product');
 
 			/** @var QueryBuilder $queryBuilder */
 			$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_category');
 			$statement = $queryBuilder
-				->select('uid', 'tx_entity_page')
+				->select('uid')
 				->from('sys_category')
 				->where(
-					$queryBuilder->expr()->eq('parent', $queryBuilder->createNamedParameter((int)$extensionConfiguration['parentMasterProductCategory'], Connection::PARAM_INT)),
+					$queryBuilder->expr()->eq('tx_foundation_identifier', $queryBuilder->createNamedParameter('entity-product-main', Connection::PARAM_STR)),
 					$queryBuilder->expr()->in('sys_language_uid', [0, -1])
 				)
 				->execute();
+			$master = $statement->fetchAssociative();
 
-			while($row = $statement->fetch()) {
-				$this->masterCategories[(int)$row['uid']] = (int) $row['tx_entity_page'];
+			if($master !== false) {
+
+				/** @var QueryBuilder $queryBuilder */
+				$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_category');
+				$statement = $queryBuilder
+					->select('uid', 'tx_entity_page')
+					->from('sys_category')
+					->where(
+						$queryBuilder->expr()->eq('parent', $queryBuilder->createNamedParameter((int) $master['uid'], Connection::PARAM_INT)),
+						$queryBuilder->expr()->in('sys_language_uid', [0, -1])
+					)
+					->execute();
+
+				while($row = $statement->fetchAssociative()) {
+					$this->masterCategories[(int)$row['uid']] = (int) $row['tx_entity_page'];
+				}
 			}
 		}
 
@@ -64,30 +80,30 @@ class RecordsXmlSitemapDataProvider {
 	 * @return array
 	 */
 	protected function defineUrl(array $data): array {
-//		if(empty($data['data']['master_category']) === true) {
-//			return $data;
-//		}
-//
-//		$pageId = $this->getMasterCategoryPageUid($data['data']['master_category']);
-//		$additionalParams = [];
-//
-//		$additionalParams = $this->getUrlFieldParameterMap($additionalParams, $data['data']);
-//		$additionalParams = $this->getUrlAdditionalParams($additionalParams);
-//
-//		$additionalParamsString = http_build_query(
-//			$additionalParams,
-//			'',
-//			'&',
-//			PHP_QUERY_RFC3986
-//		);
-//
-//		$typoLinkConfig = [
-//			'parameter' => $pageId,
-//			'additionalParams' => $additionalParamsString ? '&' . $additionalParamsString : '',
-//			'forceAbsoluteUrl' => 1,
-//		];
-//
-//		$data['loc'] = $this->cObj->typoLink_URL($typoLinkConfig);
+		if(empty($data['data']['master_category']) === true) {
+			return $data;
+		}
+
+		$pageId = $this->getMasterCategoryPageUid($data['data']['master_category']);
+		$additionalParams = [];
+
+		$additionalParams = $this->getUrlFieldParameterMap($additionalParams, $data['data']);
+		$additionalParams = $this->getUrlAdditionalParams($additionalParams);
+
+		$additionalParamsString = http_build_query(
+			$additionalParams,
+			'',
+			'&',
+			PHP_QUERY_RFC3986
+		);
+
+		$typoLinkConfig = [
+			'parameter' => $pageId,
+			'additionalParams' => $additionalParamsString ? '&' . $additionalParamsString : '',
+			'forceAbsoluteUrl' => 1,
+		];
+
+		$data['loc'] = $this->cObj->typoLink_URL($typoLinkConfig);
 
 		return $data;
 	}

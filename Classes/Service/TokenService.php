3bd3e6c60fbc634b51b6915e8a0dd45c21629864
Christian Pschorr <pschorr.christian@gmail.com>
2024-11-13 22:42:19 +0100

    TokenService gefixt -> übersetzte Datensätze erhalten einen neuen Token (bisher Kopie des Elterndatensatzes)

--- Classes/Service/TokenService.php
+++ Classes/Service/TokenService.php
@@ -3,6 +3,7 @@ namespace Ps\Entity\Service;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * Class TokenService
@@ -19,6 +20,17 @@ class TokenService {
 	 */
 	function processDatamap_afterDatabaseOperations($status, $table, $id, &$fields, &$dataHandler) {
 		if($table == 'tx_entity_domain_model_entity') {
+
+			// Uebersetzte Datensaetze bekommen vorerst die gleiche UID wie der Elterndatensatz
+			if($status === 'new') {
+				$connection = GeneralUtility::makeInstance(ConnectionPool::class)
+					->getConnectionForTable('tx_entity_domain_model_entity');
+
+				$sql = "UPDATE tx_entity_domain_model_entity SET token = MD5(" . $dataHandler->substNEWwithIDs[$id] . ") WHERE uid = " . (int) $dataHandler->substNEWwithIDs[$id];
+				$connection->executeQuery($sql);
+			}
+
+			// Fallback
 			$connection = GeneralUtility::makeInstance(ConnectionPool::class)
 				->getConnectionForTable('tx_entity_domain_model_entity');
 
